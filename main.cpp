d4dd70df108e407193c9d41d06efabe5bfb0400b
SmolichK <kostia>
2026-09-07 17:30:23 +0300

    feature/homework_one add code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "version.h"
 
 using namespace std;
 
 int main(){
-
+    
+	 cout << "build: " << APP_VERSION_STRING << endl;
     cout<<"Hello World!!";
-
+    return 0;
 }
